0bd6fd9c2b41e10872505d2c547583c5af0cb80c
Andrew Patterson <apat24@mail.sfsu.edu>
2017-10-08 20:53:23 -0700

    fixes routing from hyperlinks to info pages in index.hbs. Fixes routing to make base route to point to /fa17g15

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var lessMiddleware = require('less-middleware');
+var namespace = require('express-namespace'); 
+
 
 var index = require('./routes/index');
 var andrew = require('./routes/andrew');
@@ -13,7 +15,7 @@ var mena = require('./routes/mena');
 var norald = require('./routes/norald');
 // TODO: import your route
 var users = require('./routes/users');
-
+var router = express.Router(); 
 var app = express();
 
 // view engine setup
@@ -29,12 +31,35 @@ app.use(cookieParser());
 app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/fa17g15', index);
-app.use('/users', users);
-app.use('/fa17g15/andrew', andrew);
-app.use('/fa17g15/benedikt', benedikt); 
-app.use('/fa17g15/mena', mena); 
-app.use('/fa17g15/norald', norald);
+
+// router.use('/', function (req, res, next) {
+// 	console.log('Would you fucking work?!'); 
+// 	router.use(index);
+// 	next(); 
+// });
+
+// router.use(function(req, res, next) {
+// 	console.log("HELLO!?"); 
+
+// });
+
+// app.use('/fa17g15', router);
+
+router.use('/', index)
+router.use('/andrew', andrew);
+router.use('/benedikt', benedikt); 
+router.use('/mena', mena); 
+router.use('/norald', norald);
+
+app.use('/fa17g15', router); 
+
+// app.use('/', index);
+// app.use('/users', users);
+// app.use('/andrew', andrew);
+// app.use('/benedikt', benedikt); 
+// app.use('/mena', mena); 
+// app.use('/norald', norald);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -53,5 +78,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+// ////////////////////////////////////////
+
+
 
 module.exports = app;
