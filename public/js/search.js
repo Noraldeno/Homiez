4b13524a485f8863d521ef333003bb457d956be7
noraldeno <norald.alejo@gmail.com>
2017-10-28 18:55:30 -0700

    Fixed search bug. Added Navbar to About page

--- public/js/search.js
+++ public/js/search.js
@@ -1,20 +1,8 @@
+
 var input = document.getElementById("searchBar");
-var submit = document.getElementById("submit");
+document.getElementById("submitThis").onclick = function(){
+	console.log(input.value);
+};
 
-
-submit.addEventListener("onclick", function (event){
-	
-	console.log('it works');
-	
-});
-
-const database = require('./db/database');
-const connection = database.connect(); 
-
-connection.sync().then(function () {
-
-  database.searchListings(input);
-
-});
 
 
