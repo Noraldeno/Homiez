2020f142cb9989a36250affa8508fb3b9433a415
Norald Alejo <norald.alejo@gmail.com>
2017-11-16 13:30:25 -0800

    Added geolocation service to google maps

--- public/js/map.js
+++ public/js/map.js
@@ -1,14 +1,42 @@
 function initMap() {
-	var latitude = parseFloat($("#map").data('lat'));
-	var longitude = parseFloat($("#map").data('long'));
-    var pos = {lat: latitude, lng: longitude};
+    var pos = {lat: -34.397, lng: 150.644};
 
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 15,
         center: pos
     });
-    var marker = new google.maps.Marker({
-        position: pos,
-        map: map
+
+    var geocoder = new google.maps.Geocoder();
+
+    var address = $("#map").data('address');
+    console.log(address);
+    geocoder.geocode({'address': address}, function(results, status){
+        if (status === 'OK'){
+            map.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location
+            });
+        }
+        else{
+            alert('Geocode was not successful for the following reason ' + status);
+        }
+    });
+}
+
+function geocodeAddress(geocoder, resultsMap){
+    var address = $("#map").data('address');
+    console.log(address);
+    geocoder.geocode({'address': address}, function(results, status){
+        if (status === 'OK'){
+            resultsMap.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                map: resultsMap,
+                position: results[0].geometry.location
+            });
+        }
+        else{
+            alert('Geocode was not successful for the following reason ' + status);
+        }
     });
 }
